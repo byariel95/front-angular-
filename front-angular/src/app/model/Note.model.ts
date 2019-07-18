37f733f842b51fc38db2d97957e75f11d0784860
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-07-18 17:30:26 -0600

    added model of note

---
+++ front-angular/src/app/model/Note.model.ts
@@ -0,0 +1,8 @@
+
+export interface Note {
+    id?: string;
+    title: string;
+    content: string;
+    author: string;
+    date?: Date;
+}
--- front-angular/src/app/services/note.service.ts
+++ front-angular/src/app/services/note.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getNotes () { }
+  getNote (id){}
+  createNote (note){}
+  updateNote(id,note){}
+  deleteNote(id){}
+
+  
 }
