451f78c61ed310f99a3017faa1c6f6cfd5b01758
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-07-18 17:06:07 -0600

    added navbar

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NoteListComponent }from './components/note-list/note-list.component'
+import {NoteFormComponent} from './components/note-form/note-form.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: NoteListComponent
+  },
+  {
+    path: 'note',
+    component:  NoteListComponent
+  },
+  {
+    path: 'note/create',
+    component: NoteFormComponent
+  },
+  {
+    path: 'note/edit/:id',
+    component: NoteFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
